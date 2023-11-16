c200324e6f45097ca084fcb0605082f9a7854478
shruthiamu14 <shruthiammu470@gmail.com>
2023-11-16 15:40:08 +0530

    first commit

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // You can add code to handle form submission, e.g., sending an email or storing data.
+    alert("Form submitted!");
+});
